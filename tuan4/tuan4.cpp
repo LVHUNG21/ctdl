e50a14629e5fdc9b034a2e4a7c1f671fdb915e82
Sinh Vien TLU ( Pass:123456 ) <hung0911248587@gmail.com>
2023-05-20 17:06:50 +0700

    update

--- tuan4/tuan4.cpp
+++ tuan4/tuan4.cpp
@@ -55,16 +55,21 @@ public:
     }
     void insert(const E &newdata, int a)
     {
-        if(head==nullptr){
+        if (head == nullptr)
+        {
             addFirst(newdata);
         }
-        if(a<0 || a>size) return;
-        if(a==size){addLast(newdata);}
+        if (a < 0 || a > size)
+            return;
+        if (a == size)
+        {
+            addLast(newdata);
+        }
         Node<E> *new_node = new Node<E>(newdata);
         Node<E> *current = head;
         Node<E> *current2 = new Node<E>();
         int count = 1;
-        while (count < a-1)
+        while (count < a - 1)
         {
             current = current->next;
             count++;
@@ -75,12 +80,79 @@ public:
         current2->prev = new_node;
         new_node->next = current2;
         size++;
-        
+
         // if(index<0 || index>size){
 
         // }
+    }
+    void deleteFirst()
+    {
+        Node<E> *node = new Node<E>();
+        node = head;
+        if (head == nullptr)
+            return;
+        if (tail->prev == nullptr)
+        {
 
+            delete head;
+            head = tail = nullptr;
+            return;
+        }
+        head = head->next;
+        head->next->prev = nullptr;
+        delete node;
 
+        size--;
+    }
+    void deleteLast()
+    {
+        Node<E> *node = new Node<E>();
+        node = tail;
+        if (tail == nullptr)
+            return;
+        if (tail->prev == nullptr)
+        {
+            delete tail;
+            head = tail = nullptr;
+            return;
+        }
+        tail->prev->next = nullptr;
+        delete node;
+        tail = tail->prev;
+        size--;
+    }
+    void deleteAtIndex(int a)
+    {
+        Node<E> *current = head;
+        Node<E> *current2 = new Node<E>();
+
+        if (a < 0 || a > size)
+        {
+            cout << "khong hop le" << endl;
+        }
+        else if (a == 0)
+            deleteFirst();
+        else if (a == size)
+            deleteLast();
+
+        else
+        {
+
+            int count = 1;
+            while (count < a)
+            {
+                // if(count==size){
+                //     break;
+                // }
+                current = current->next;
+                count++;
+            }
+            current->next->prev = current->prev;
+            current->prev->next = current->next;
+            current2 = current;
+            delete current2;
+            size--;
+        }
     }
     void print() const
     {
@@ -100,15 +172,19 @@ public:
         }
     }
 };
-
 int main()
 {
     DList<int> list;
     list.addFirst(5);
     list.addLast(8);
-    list.addLast(8);
-    list.addLast(6);
+    list.addLast(7);
+    list.addLast(5);
+    // list.addLast(6);
     list.insert(1, 3);
+    // list.deleteFirst();
+    // list.deleteAtIndex(3);
+    list.deleteAtIndex(4);
+    // list.deleteLast();
     list.print(); // Print the list (outputs: 5)
     return 0;
 }
