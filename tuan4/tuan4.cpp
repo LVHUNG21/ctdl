647c874f3b8f08a64d45cbe04ba14b0000891972
Sinh vien(pass:123456) <hung0911248587@gmail.com>
2023-05-26 15:52:49 +0700

    update

--- tuan4/tuan4.cpp
+++ tuan4/tuan4.cpp
@@ -21,7 +21,29 @@ protected:
 
 public:
     DList() : head(nullptr), tail(nullptr), size(0) {}
-
+    // DList(const DList<E>& DList2 ){
+    //     Node<E>* current=DList2.head;
+    //     // if(DList2.head ! =nullptr){
+    //         while (current!=nullptr)
+    //         {
+    //        addLast(current->data);
+    //        current=current->next;
+    //         }
+    //         // }
+    //     }
+    //  // the copy contructor
+    DList(const DList<E> &DList2) : head(nullptr), tail(nullptr), size(0)
+    {
+        if (DList2.head != nullptr)
+        {
+            Node<E> *current = DList2.head;
+            while (current != nullptr)
+            {
+                addLast(current->data);
+                current = current->next;
+            }
+        }
+    }
     void addFirst(const E &newdata)
     {
         Node<E> *new_node = new Node<E>(newdata);
@@ -117,8 +139,8 @@ public:
             return;
         }
         tail->prev->next = nullptr;
-        delete node;
         tail = tail->prev;
+        delete node;
         size--;
     }
     void deleteAtIndex(int a)
@@ -138,7 +160,7 @@ public:
         else
         {
 
-            int count = 1;
+            int count = 0;
             while (count < a)
             {
                 // if(count==size){
@@ -154,6 +176,68 @@ public:
             size--;
         }
     }
+    void removeKey(const E &e)
+    {
+        Node<E> *current = new Node<E>();
+        current = head;
+        int count = 0;
+        while (current != nullptr)
+        {
+            if (current->data == e)
+            {
+                deleteAtIndex(count);
+                count--;
+                // break;
+            }
+            else
+            {
+                count++;
+            }
+            current = current->next;
+        }
+    }
+    void replace(int i, const E &e)
+    {
+        Node<E> *current = new Node<E>();
+        current = head;
+        int count = 1;
+        while (current != nullptr)
+        {
+            if (count == i)
+            {
+                current->data = e;
+                break;
+            }
+            current = current->next;
+            count++;
+        }
+    }
+    void replaceKey(const E &e, const E &e1)
+    {
+        Node<E> *current = new Node<E>();
+        current = head;
+        while (current != nullptr)
+        {
+            if (current->data == e)
+            {
+                current->data = e1;
+            }
+            current = current->next;
+        }
+    }
+    // void clear(){
+    //     Node<E> * current=new Node<E>();
+    //     current=head;
+    //     while(current!=nullptr){
+    //         Node<E> * temp=new Node<E>();
+    //         temp=current;
+    //         current=current->next;
+    //         delete temp;
+    //     }
+    //     head=tail=nullptr;
+    //     size=0;
+    // }
+
     void print() const
     {
         Node<E> *current = head;
@@ -171,20 +255,63 @@ public:
             cout << current->data << endl;
         }
     }
+    const DList<E> &operator=(const DList<E> &DList2)
+    {
+        // if (this == &DList2)
+        //     return *this;
+
+        // // Clear current list
+        Node<E> *current = head;
+        // while (current != nullptr)
+        // {
+        //     Node<E> *temp = current;
+        //     current = current->next;
+        //     delete temp;
+        // }
+        // head = tail = nullptr;
+        // size = 0;
+
+        // Copy elements from DList2
+        current = DList2.head;
+        while (current != nullptr)
+        {
+            addLast(current->data);
+            current = current->next;
+        }
+
+        return *this;
+    }
 };
+
 int main()
 {
     DList<int> list;
-    list.addFirst(5);
-    list.addLast(8);
+    list.addFirst(1);
+    list.addLast(9);
     list.addLast(7);
-    list.addLast(5);
+    list.addLast(9);
+    list.addLast(9);
     // list.addLast(6);
-    list.insert(1, 3);
+    // list.insert(1, 3);
+
     // list.deleteFirst();
-    // list.deleteAtIndex(3);
-    list.deleteAtIndex(4);
-    // list.deleteLast();
+    // list.deleteAtIndex(0);
+
+    list.removeKey(9);
+    // list.replace(1,21);
+    // list.replaceKey(8,21);
+
     list.print(); // Print the list (outputs: 5)
+    // list.clear();
+
+    // list.addFirst(2);
+    // list.print();
+
+    DList<int> list2(list);
+    list2.print();
+    DList<int> list3;
+    list3 = list2;
+    list3.print();
+
     return 0;
 }
